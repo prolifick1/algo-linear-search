b5e68652b0fda40f1a2da4442dc67509c3617c23
Luis Manzo <manzosk3@Luiss-MacBook-Air.local>
2022-05-25 21:50:40 -0500

    initial commit, TODO: verify solution

--- js/linearSearch.js
+++ js/linearSearch.js
@@ -4,6 +4,23 @@ for (let i = 1; i <= 1000; i++) {
 }
 
 exports.linearSearch = function(valueToFind, arrayToSearchThrough) {
-
+  for(let i = 0; i < arrayToSearchThrough.length; i++) {
+    if(valueToFind === arrayToSearchThrough[i]) {
+      return i;
+    }
+  }
+  return undefined;
 };
 
+exports.linearSearchGlobal = function(valueToFind, arrayToSearchThrough) {
+  let res = [];
+  for(let i = 0; i < arrayToSearchThrough.length; i++) {
+    if(valueToFind === arrayToSearchThrough[i]) {
+      console.log(i);
+      res.push((function() {
+        return i;
+      })())
+    }
+  }
+  return res;
+}
